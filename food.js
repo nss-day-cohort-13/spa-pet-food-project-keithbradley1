5bca3f731f5465db28a9bdd48c8471416df03045
Keith <keithbradley1@msn.com>
2016-05-03 12:50:18 -0500

    still needs a couple little tweaks

--- food.js
+++ food.js
@@ -1,26 +1,26 @@
 
 var dogRequest = new XMLHttpRequest() ;
 var catRequest = new XMLHttpRequest() ;
-var dogLoc = document.getElementById("dogFood")
-var catLoc = document.getElementById("catFood")
+var dogLoc = document.getElementById("dogFood");
+var catLoc = document.getElementById("catFood");
 
 
-function dogLists(argument) {
+function dogLists() {
   var dogData = JSON.parse(this.responseText).dog_brands ;
   for (var i = 0; i < dogData.length; i++) {
     // console.log( dogData[i] ) ;
-    var callData = "<h2>" + dogData[i].name + "</h2>"
+    var callData = "<h2>" + "<p>" + "Dog Food Brands" + "</p>" + dogData[i].name + "</h2>";
 
     var dogType = dogData[i].types ;
     for (var k = 0; k < dogType.length; k++) {
       var dogVol = dogType[k].volumes;
-      var callType = "<h4>" + dogType[i].type + "</h4>"
+      var callType = "<h4>" + dogType[i].type + "</h4>";
 
       // console.log( dogVol);
       var callNamePrice = "";
       for (var b = 0; b < dogVol.length; b++) {
         dogVol[b];
-        callNamePrice += "<p>" + dogVol[b].name + "</p>" + "<p>$" + dogVol[b].price + "</p>"
+        callNamePrice += "<p>" + dogVol[b].name + ":" + " $" + dogVol[b].price + "</p>";
 
         // console.log( dogVol[b].name);
       }
@@ -30,30 +30,25 @@ function dogLists(argument) {
   };
 };
 
-
-
-
-
-function catLists(argument) {
+function catLists() {
   var catData = JSON.parse(this.responseText).cat_brands ;
   console.log(callData);
   for (var i = 0; i < catData.length; i++) {
     // console.log( catData[i] ) ;
-    var callData = "<h2>" + catData[i].name + "</h2>"
+    var callData = "<h2>" + "<p>" + "Cat Food Brands" + "</p>"  + catData[i].name + "</h2>";
 
     var catType = catData[i].types ;
     for (var k = 0; k < catType.length; k++) {
       var catVol = catType[k].volumes;
-      var callType = "<h4>" + catType[i].type + "</h4>"
+      var callType = "<h4>" + catType[i].type + "</h4>";
 
       // console.log( catVol);
       var callNamePrice = "";
       for (var b = 0; b < catVol.length; b++) {
         catVol[b];
-        callNamePrice += "<p>" + catVol[b].name + "</p>" + "<p>$" + catVol[b].price + "</p>"
-
+        callNamePrice += "<p>" + catVol[b].name + ": " + " $" + catVol[b].price + "</p>";
         // console.log( catVol[b].name);
-      }
+      };
     };
       catLoc.innerHTML += "<div>" + callData + callType  + callNamePrice + "</div>";
 
